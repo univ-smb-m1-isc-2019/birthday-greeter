167b4e4d02bd5bc42367e055bcecfcdf6e5fff69
Alexandre <alascenci@gmail.com>
2019-12-02 17:46:55 +0100

    Génération du projet Maven

---
+++ src/main/java/univ-smb-m1-isc-2019/App.java
@@ -0,0 +1,13 @@
+package univ-smb-m1-isc-2019;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/univ-smb-m1-isc-2019/AppTest.java
@@ -0,0 +1,20 @@
+package univ-smb-m1-isc-2019;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

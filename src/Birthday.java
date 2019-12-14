033c4972cbec6dc4d5818c33a0183f2b205b5d4a
Khelifi Sabrina <sabrina1.1khelifi@gmail.com>
2019-12-14 04:50:02 +0100

    Happy BirZday

---
+++ src/Birthday.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Birthday {
+
+    public static void main(String[] args){
+        //execution de toutes les fonctions dans SayHappyBirthday
+        SayHappyBirthday Happy = new SayHappyBirthday();
+
+
+    }
+}

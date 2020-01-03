cb4b2f9c720af676ea4f9c88cbaada15c7cbcd0b
YannDubost <35998187+YannDubost@users.noreply.github.com>
2020-01-03 17:30:30 +0100

    Modification commentaires

--- src/main/java/fr/univsmb/isc/SendEMail.java
+++ src/main/java/fr/univsmb/isc/SendEMail.java
@@ -14,10 +14,6 @@ public class SendEMail
     /*git rebase puis faire le refactoring
     *
     * */
-
-
-
-
     private ArrayList<String> listToSend;/*Liste des personnes dont c'est l'anniversaire aujourd'hui*/
     private ArrayList<String> listFromFile;/*Liste de toutes les personnes du fichier*/
     private String fileName;/*Nom du fichier dans lequel sont stoquees les informations*/
@@ -27,10 +23,10 @@ public class SendEMail
     public SendEMail()
     {
         fileName = "humans.txt";/*Le fichier dans le src du projet*/
-        listToSend = new ArrayList<>();
         listFromFile = new ArrayList<>();
         listFromFile = MyFileReader.readFile(fileName);/*On recupere les informations du fichier*/
         birthday = new Birthday();
+        listToSend = birthday.setListBirthday(listFromFile);/*On charge la liste des personne dont c'est l'anniversaire aujourd'hui*/
 
     }
 
@@ -38,7 +34,7 @@ public class SendEMail
         /*Procedure d'envoie de mail a toutes les personnes dont c'est l'anneversaire aujourd'hui*/
     {
         String[] info;
-        listToSend = birthday.setListBirthday(listFromFile);/*On charge la liste des personne dont c'est l'anniversaire aujourd'hui*/
+
         for(String s : listToSend)
             /*On envoie un mail a chacune de ces personnes*/
         {
@@ -46,12 +42,4 @@ public class SendEMail
             System.out.println("Bon anniversaire " + info[0] + " " + info[1]);/*Envoie de mail simule par un print*/
         }
     }
-
-
-
-
-
-
-
-
 }

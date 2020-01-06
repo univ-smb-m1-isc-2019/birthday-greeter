ceed7c078bc329bdf9f5ccf86be6a15f4c85dab3
Khelifi Sabrina <sabrina1.1khelifi@gmail.com>
2020-01-06 14:02:56 +0100

    explication de l'objectif de l'application comme mentionné dans commentaire

--- src/Birthday.java
+++ src/Birthday.java
@@ -7,8 +7,9 @@ public class Birthday {
 
     public static void main(String[] args){
         //execution de toutes les fonctions dans SayHappyBirthday
+        //souhaiter un joyeux anniversaire à tous les utlilisateurs dont c'est l'anniversaire
         SayHappyBirthday Happy = new SayHappyBirthday();
-
+            Happy.HappyBirthday();
 
     }
 }
